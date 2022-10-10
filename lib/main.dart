ad42f6f4c9e78ccac7c6efc80cf9a9842e3c32ee
Yusuf KIZILKAYA <kzlkayayusuf@outlook.com>
2022-10-10 14:04:57 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ChatApp());
+}
+
+class ChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Chat App!!!",
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Button clicked!');
+        },
+      ),
+      body: Text("Hello la World!"),
+    );
+  }
+}
+
