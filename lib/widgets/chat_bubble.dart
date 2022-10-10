c99a1001dc31a3c5fa7a4aa7cfc8cbcccecfa8f1
Yusuf KIZILKAYA <kzlkayayusuf@outlook.com>
2022-10-10 16:17:14 +0300

    Creating your own custom widget

---
+++ lib/widgets/chat_bubble.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ChatBubble extends StatelessWidget {
+  const ChatBubble({Key? key, required this.alignment,required this.message}) : super(key: key);
+
+  final String message;
+  final Alignment alignment;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: alignment,
+      child: Container(
+        padding: EdgeInsets.all(24),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              '$message',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+            Image.network(
+                'https://th.bing.com/th/id/OIP.FsGhfXy7oinbrRqBwcWeigHaEj?w=294&h=181&c=7&r=0&o=5&pid=1.7'),
+          ],
+        ),
+        margin: EdgeInsets.all(50),
+        decoration: BoxDecoration(
+          color: Colors.green,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(12),
+            topRight: Radius.circular(12),
+            bottomLeft: Radius.circular(12),
+          ),
+        ),
+      ),
+    );
+  }
+}
