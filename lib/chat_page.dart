a0a28f83a0930c035e25fc3b4b7959ddd9463924
Yusuf KIZILKAYA <kzlkayayusuf@outlook.com>
2022-10-11 11:38:23 +0300

    Reading data from a JSON file

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,37 +1,41 @@
+import 'dart:convert';
+
 import 'package:chat_app/models/chat_message_entity.dart';
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
    ChatPage({Key? key}) : super(key: key);
 
-  List<ChatMessageEntity> _messages= [
-    ChatMessageEntity(
-      id: '1',
-      text: 'your message 1',
-      createdAt: DateTime.now().microsecondsSinceEpoch,
-      author: Author(userName: 'zeynep'),
-    ),
-    ChatMessageEntity(
-      id: '2',
-      text: 'your message 2',
-      createdAt: DateTime.now().microsecondsSinceEpoch,
-      author: Author(userName: 'zeynep'),
-    ),
-    ChatMessageEntity(
-      id: '3',
-      text: 'your message 3',
-      createdAt: DateTime.now().microsecondsSinceEpoch,
-      author: Author(userName: 'zeynep'),
-    ),
-    ChatMessageEntity(
-      id: '4',
-      text: 'your message 4',
-      createdAt: DateTime.now().microsecondsSinceEpoch,
-      author: Author(userName: 'mustafa'),
-    ),
-  ];
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  //initiate state of messages
+  List<ChatMessageEntity> _messages= [];
+
+  _loadInitialMessages() async{
+    final response = await rootBundle.loadString('assets/mock_messages.json');
+
+    final List<dynamic> decodedList= jsonDecode(response) as List;
+    final List<ChatMessageEntity> _chatMessages= decodedList.map((listItem) => ChatMessageEntity.fromJson(listItem)).toList();
+
+    print(_chatMessages.length);
+
+    //final state of the messages
+    setState(() {
+      _messages=_chatMessages;
+    });
+  }
+
+  @override
+  void initState() {
+    _loadInitialMessages();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
